4d43c058feba4c3b292d515f831e132a8b634c63
sky1o1 <aakash.dangol101@gmail.com>
2021-01-26 16:04:28 +0545

    [updated] textfield size and misc

--- src/store/reducer/skills.js
+++ src/store/reducer/skills.js
@@ -1,16 +1,24 @@
-import { createAction, handleAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 export const setSkills = createAction('SET_SKILLS');
+export const setAbilities = createAction('SET_ABILITIES');
 
 const INITIAL_VALUES = {
+    abilities: '',
     skillsList : []   
 }
 
-const skillsReducer = handleAction(
-    'SET_SKILLS',
-    (state, action) =>( {
-        skillsList: [...state.skillsList, action.payload]
-    }),
+const skillsReducer = handleActions(
+        {
+            'SET_ABILITIES':
+            (state, action) => {
+                return {...action.payload}
+            } ,
+            'SET_SKILLS':
+            (state, action) =>( {
+                skillsList: [...state.skillsList, action.payload]
+            }),
+    },
     INITIAL_VALUES
 )
 
