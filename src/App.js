4c41451db1d9b94f1d8504a9edc38f3a670d2d10
sky1o1 <aakash.dangol101@gmail.com>
2021-02-15 16:11:05 +0545

    [add] app bar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React, {useEffect} from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';  
-import {useDispatch} from 'react-redux';
-import PrivateRoute from './common/PrivateRoute';  
-import {setAuthenticate} from './store/reducer/authenticate';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './common/PrivateRoute';
+import { setAuthenticate } from './store/reducer/authenticate';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import Sidebar from '../src/views/Sidebar';
 import Index from '../src/views/Index';
 import ScrollToTop from '../src/services/ScrollToTop';
-import {auth} from './services/firebase/config';
+import { auth } from './services/firebase/config';
 import Login from './views/auth/Login';
 import Home from './views/home/Home';
 import Resume from './views/resume/Resume';
@@ -33,170 +15,44 @@ import Services from './views/services/Services';
 import Contact from './views/Contact';
 import Projects from './views/project/Projects';
 import Draggable from './views/project/ProjectTest';
+import AppBarMenu from './views/AppBar';
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginRight: drawerWidth,
-  },
-  title: {
-    flexGrow: 1,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-start',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginRight: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginRight: 0,
-  },
-}));
 
 function App() {
-  const classes = useStyles()
- const dispatch = useDispatch()
-  const theme = useTheme();
- const [open, setOpen] = React.useState(false);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     auth.onAuthStateChanged(async user => {
-      if(user){
+      if (user) {
         dispatch(setAuthenticate(true))
-      }else{
+      } else {
         dispatch(setAuthenticate(false))
       }
     })
   })
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
   return (
-<>
-<div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap className={classes.title}>
-            EV-TAE
-          </Typography>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerOpen}
-            className={clsx(open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="right"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-      </Drawer>
-    </div>
-
-    <div id="lonon-page"> <a href="#" class="js-lonon-nav-toggle lonon-nav-toggle"><i></i></a>
-
-  <Router>
-  <ScrollToTop/>
-      <Switch>
-      <Route exact path='/login'>
-        <Login />
-      </Route>
-    <PrivateRoute component={Sidebar}/>
-    <PrivateRoute path='/' component={Index} />
-    <PrivateRoute path='/home' component={Home} />
-    <PrivateRoute path='/resume' component={Resume} />
-    <PrivateRoute path='/portfolio' component={Projects} />
-    <PrivateRoute path='/drag' component={Draggable} />
-    <PrivateRoute path='/services' component={Services} />
-    <PrivateRoute path='/contact' component={Contact} />
-
-
-    </Switch>
-  </Router>
- </div>
-</>
+    <>
+      <AppBarMenu />
+      <div id="lonon-page"> <a href="#" class="js-lonon-nav-toggle lonon-nav-toggle"><i></i></a>
+        <Router>
+          <ScrollToTop />
+          <Switch>
+            <Route exact path='/login'>
+              <Login />
+            </Route>
+            <PrivateRoute component={Sidebar} />
+            <PrivateRoute path='/' component={Index} />
+            <PrivateRoute path='/home' component={Home} />
+            <PrivateRoute path='/resume' component={Resume} />
+            <PrivateRoute path='/portfolio' component={Projects} />
+            <PrivateRoute path='/drag' component={Draggable} />
+            <PrivateRoute path='/services' component={Services} />
+            <PrivateRoute path='/contact' component={Contact} />
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
